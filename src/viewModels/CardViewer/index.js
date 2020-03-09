91403211fce43bfac94af7eb1e0e1eb00d24e3d4
Brian Taggart <tp_slc_brtaggart@msn.com>
2020-03-09 17:49:25 -0600

    Getting player data from server

--- src/viewModels/CardViewer/index.js
+++ src/viewModels/CardViewer/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import axios from 'axios';
 import Search from '../../components/Main/Search'
 import Card from '../../components/Main/Card'
 import "./styles.css";
@@ -8,12 +9,38 @@ import "./styles.css";
 
 class CardViewer extends Component{
 
+    constructor(props){
+        super(props);
+        this.state = {isLoading: true, playerData: [], showError: false};
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:3008/players')
+        .then(this.assignPlayerData)
+        .catch(this.processPlayerDataError);        
+    }
+
     render (){
         return <div className='container'>
             <p className='title'>NBA Interview</p>
-            <Search />
-            <Card playerName='Gordon "Snake" Hayward' playerImageUrl='http://localhost:3008/gordon_hayward.png' playerTeamName='Boston Celtics' />
+            {this.state.showError && <span> There was an error. Normally this would be in red or something. </span>}
+            {this.state.isLoading && <span> Loading...</span>}
+            {!this.state.isLoading && <>
+                <Search />
+                <Card playerName='Gordon "Snake" Hayward' playerImageUrl='http://localhost:3008/gordon_hayward.png' playerTeamName='Boston Celtics' />
+            </>}
         </div>
+    }
+
+    //Because anonymous functions consume more memory every time the component mounts or renders. This will do it once per class
+    assignPlayerData= (result) => {        
+        console.log(result);          
+        this.setState({isLoading:false, playerData: result.data});
+    }
+
+    processPlayerDataError = (error) => {
+        console.log(error);        
+        this.setState({showError: true});
     }
 }
 
